81b33b6e67a12cb8b5626501d9737dc7200099cc
Jadude234 <157072153+Jadude234@users.noreply.github.com>
2025-04-10 15:47:50 -0500

    GameManager
    Added dictionary to GameManager for switches

---
+++ Assets/Scripts/ToDo.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ToDo : MonoBehaviour
+{
+    // Not a script, but a conviently placed ToDo
+
+    /*
+    Add basic scripts for switches, buttons, walls, and other interactables
+    Have them with publically editable strings for their names in the GameManager Dictionary
+    Do not use set active, instead turn off the collisioin and visibility
+    Make sure to call the SetFlag Function in GameManager in Enable
+
+    Make script that tests Enable
+    */
+}

d791fbc3f102fba185cdf1378c7d1326ccae34cf
Trevor Rose <trevorrose@outlook.com>
2025-04-16 19:17:01 -0500

    AddedCheckPoint Script

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public GameObject model;
+    public bool FlagIsRaised = false; 
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (FlagIsRaised) 
+        {
+            // Has set this checkpoint already
+            return; 
+        }
+
+        PlayerManager player = other.gameObject.GetComponentInParent<PlayerManager>();
+        if (player != null) 
+        {
+            FlagIsRaised = true;
+            GameManager.Instance.SetCheckPoint(gameObject);
+            RaiseTheFlag();
+        }
+
+    }
+
+    public void RaiseTheFlag()
+    {
+        
+    }
+
+}

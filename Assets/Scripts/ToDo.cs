879d1a247062bd0e9004569ca59d7fb43b178eb0
Jadude234 <157072153+Jadude234@users.noreply.github.com>
2025-04-23 20:50:39 -0500

    Button
    Added triggerCount to button script
    Allowing for object to stay on button and keep it active

--- Assets/Scripts/ToDo.cs
+++ Assets/Scripts/ToDo.cs
@@ -5,10 +5,7 @@ public class ToDo : MonoBehaviour
     // Not a script, but a conviently placed ToDo
 
     /*
-    Add basic scripts for switches, buttons, walls, and other interactables
-    Have them with publically editable strings for their names in the GameManager Dictionary
-    Do not use set active, instead turn off the collisioin and visibility
-    Make sure to call the SetFlag Function in GameManager in Enable
+    add sprites to interactable objects
 
     Make script that tests Enable
 

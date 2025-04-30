508bcc69b6f82bcf66912d8720bcf4bb692284ee
Seleena <127824931+3leorising@users.noreply.github.com>
2025-04-30 18:33:43 -0500

    Camera and checkpoint
    Script made to follow Player
    
    Check points indeed work

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 1, -1);
+    }
+}
